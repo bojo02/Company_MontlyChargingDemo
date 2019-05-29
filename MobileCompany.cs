0584c097071f93be4b7ccb61cde65dad3cfccb9a
bojo02 <bojo1810@gmail.com>
2019-05-29 18:15:24 +0300

    Create MobileCompany

---
+++ MobileCompany.cs
@@ -0,0 +1,20 @@
+﻿namespace MontlyChargeDemo
+{
+    public class MobileCompany
+    {
+        private Person[] _clients;
+
+        public MobileCompany(Person[] clients)
+        {
+            this._clients = clients;
+        }
+
+        public void MontlyCharge(decimal money)
+        {
+            for(int i = 0; i < _clients.Length; i++)
+            {
+                _clients[i].Account.Withdraw(money);
+            }
+        }
+    }
+}
