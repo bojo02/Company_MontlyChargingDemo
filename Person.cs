56d9995d65f0bfe575f0f629518c9bff67c08060
bojo02 <bojo1810@gmail.com>
2019-05-29 18:15:32 +0300

    Create Person

---
+++ Person.cs
@@ -0,0 +1,30 @@
+﻿namespace MontlyChargeDemo
+{
+    public class Person
+    {
+        public string FullName { get; set; }
+
+        public BankAccount Account { get; set; }
+
+        public GSM Phone { get; set; }
+
+        public Person(string FullnName, BankAccount bankAccount, GSM phone)
+        {
+            this.FullName = FullName;
+
+            this.Account = bankAccount;
+
+            this.Phone = phone;
+
+            //Subscribing all people by default
+            this.Account.OnBankAccountChanged += Account_OnBankAccountChanged; 
+        }
+
+        private void Account_OnBankAccountChanged(decimal balance, decimal amount, bool success)
+        {
+            string message = "Curent balance: " + balance + ",\ntaken amount: " + amount + ",\nsuccess: " + success;
+
+            Phone.SendMessage(message);
+        }
+    }
+}
