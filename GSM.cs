f82396a61304479cd6efdbf32bfb68a898425660
bojo02 <bojo1810@gmail.com>
2019-05-29 18:15:14 +0300

    Create GSM

---
+++ GSM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MontlyChargeDemo
+{
+    public class GSM
+    {
+        public string Number { get; set; }
+
+        public GSM(string number)
+        {
+            this.Number = number;
+        }
+
+        public void SendMessage(string message)
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Message: " + message);
+            Console.WriteLine("-----------------------------");
+        }
+    }
+}
