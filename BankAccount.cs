65911cce28e39bcf05bd6a77284aa08a9145d95d
bojo02 <bojo1810@gmail.com>
2019-05-29 18:15:06 +0300

    Create BankAccount

---
+++ BankAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace MontlyChargeDemo
+{
+    public delegate void WithdrawEventHandler(decimal balance, decimal amount, bool success);
+    public class BankAccount
+    {
+        public event WithdrawEventHandler OnBankAccountChanged;
+
+        public decimal Balance { get; set; }
+
+        public void Deposit(decimal amount)
+        {
+            this.Balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            if(this.Balance - amount >= 0)
+            {
+                this.Balance -= amount;
+
+                this.OnBankAccountChanged(this.Balance, amount, true);
+            }
+            else
+            {
+                this.OnBankAccountChanged(this.Balance, amount, false);
+            }
+        }
+
+    }
+}
